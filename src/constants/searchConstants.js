7d7eac9fb29d29dcdb72dd1a771adbff38d9a97b
Veerash Palanichamy <veeresh.532000@gmail.com>
2024-08-05 20:59:37 -0400

    clearing repo

--- src/constants/searchConstants.js
+++
@@ -1,4 +0,0 @@
-//constants for searching items
-export const ITEM_SEARCH_REQUEST = 'ITEM_SEARCH_REQUEST';
-export const ITEM_SEARCH_SUCCESS = 'ITEM_SEARCH_SUCCESS';
-export const ITEM_SEARCH_FAIL = 'ITEM_SEARCH_FAIL';
